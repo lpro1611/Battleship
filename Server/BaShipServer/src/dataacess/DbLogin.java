eadc030a951a2467c24e20e6627bb0a805eda337
Diogo <diogodinisf@gmail.com>
2016-11-12 22:40:42 +0000

    fazer o close de PrepareStatement e de ResultSet

--- Server/BaShipServer/src/dataacess/DbLogin.java
+++ Server/BaShipServer/src/dataacess/DbLogin.java
@@ -5,7 +5,6 @@
  */
 package dataacess;
 
-import com.sun.org.apache.xml.internal.utils.WrongParserException;
 import exceptions.DuplicatedNameException;
 import exceptions.NotFoundException;
 import exceptions.WrongPasswordException;
@@ -22,20 +21,20 @@ public class DbLogin {
     public int registerPlayer(String email, String name, String pass, Properties props) throws SQLException, DuplicatedNameException, NotFoundException {
         Connection con = DbUtils.openConnection(props);
         String insertPlayer = "INSERT INTO player (email, name, pass) values (?, ?, ?)";
-        PreparedStatement prepSt = con.prepareStatement(insertPlayer);
-        prepSt.setString(1, email);
-        prepSt.setString(2, name);
-        prepSt.setString(3, pass);
         
         try {
             if (checkIfNameExists(name, con)) {
                 throw new DuplicatedNameException();
             }
 
-            prepSt.executeUpdate();
+            try (PreparedStatement prepSt = con.prepareStatement(insertPlayer)) {
+                prepSt.setString(1, email);
+                prepSt.setString(2, name);
+                prepSt.setString(3, pass);
+                prepSt.executeUpdate();
+            }
             Pair<Integer, String> player = getPlayerByName(name, con);
             return player.getKey();
-            
         } finally {
             DbUtils.closeConnection(con);
         }
@@ -50,7 +49,6 @@ public class DbLogin {
             if (player.getValue().equals(pass)) {
                 return player.getKey();
             }
-            
             throw new WrongPasswordException();
         } finally {
             DbUtils.closeConnection(con);
@@ -59,23 +57,24 @@ public class DbLogin {
     
     private Pair<Integer, String> getPlayerByName(String name, Connection con) throws SQLException, NotFoundException {
         String selectPlayer = "SELECT id, pass FROM player WHERE name = ?";
-        PreparedStatement prepSt = con.prepareStatement(selectPlayer);
-        prepSt.setString(1, name);
-        ResultSet rs = prepSt.executeQuery();
-
-        if (rs.next()) {
-            return new Pair<Integer, String>(rs.getInt("id"), rs.getString("pass"));
+        try (PreparedStatement prepSt = con.prepareStatement(selectPlayer)) {
+            prepSt.setString(1, name);
+            try (ResultSet rs = prepSt.executeQuery()) {
+                if (rs.next()) {
+                    return new Pair<Integer, String>(rs.getInt("id"), rs.getString("pass"));
+                }
+            }
         }
-
         throw new NotFoundException();
     }
     
     private boolean checkIfNameExists(String name, Connection con) throws SQLException {
         String checkPlayer = "SELECT id FROM player WHERE name = ?";
-        PreparedStatement prepSt = con.prepareStatement(checkPlayer);
-        prepSt.setString(1, name);
-        ResultSet rs = prepSt.executeQuery();
-
-        return rs.next();
+        try (PreparedStatement prepSt = con.prepareStatement(checkPlayer)) {
+            prepSt.setString(1, name);
+            try (ResultSet rs = prepSt.executeQuery()) {
+                return rs.next();
+            }
+        }
     }
 }
